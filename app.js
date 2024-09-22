76a51de97123a5cf992de0fa489ce0c4c142d31e
AlanGimenez <gimenezalan00@gmail.com>
2024-09-22 19:34:39 -0300

    add a timeout of 60 secons

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.json("Hola").status(200));
+app.get("/", (req, res) => {
+    setTimeout(() => {
+        res.json("Hola").status(200)
+    }, 60000)
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
