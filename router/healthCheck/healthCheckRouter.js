c36ec66256105cae1b2e905ac5d2cff99bc7ae23
AlanGimenez <gimenezalan00@gmail.com>
2024-10-13 08:07:52 -0300

    add message in healthCheck endpoint

--- router/healthCheck/healthCheckRouter.js
+++ router/healthCheck/healthCheckRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const accountService = require('../../services/accounts/accountService');
 
 router.get('/', async (req, res) => {
-    res.status(200).json();
+    res.status(200).json({"message": "App running"});
 })
 
 module.exports = router
